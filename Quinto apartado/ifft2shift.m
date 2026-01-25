37e0ae15c07bc448c0695e29fd406d7444044143
LabOpticaAplicada <lab.optica.aplicada@gmail.com>
2026-01-25 17:24:45 +0100

    Add files via upload

---
+++ Quinto apartado/ifft2shift.m
@@ -0,0 +1,9 @@
+function F = ifft2shift(f)
+% Realiza la transformada de Fourier inversa bidimensional desplazando los
+% cuadrantes antes y después de la transformada. Es decir está considerando
+% el centro de coordenadas igual al centro de la matriz
+%
+% Sintaxis
+%   F = fft2shift(f);
+ 
+F = fftshift( ifft2 (fftshift (f) ) );
